f6b454b75145bf4b342ccbcf1e7a1531163a673a
马兆铿 <810768333@qq.com>
2023-11-06 23:47:38 +0800

    🌟 Match file names with appropriate icons

--- src/components/PageLayout/FolderMenu.tsx
+++ src/components/PageLayout/FolderMenu.tsx
@@ -1,7 +1,9 @@
 import React, { memo, ReactNode } from 'react'
 import { Tree } from '@arco-design/web-react'
 import { ConfigProviderProps } from '../config'
-import { IconCode, IconFolder } from '@arco-design/web-react/icon'
+import {
+  IconCode, IconCodeBlock, IconCommon, IconDriveFile, IconFilePdf, IconFolder
+} from '@arco-design/web-react/icon'
 // var
 
 interface Props extends ConfigProviderProps {
@@ -26,13 +28,32 @@ function createTreeNode(file: Obj, icon: ReactNode) {
   }
 }
 
+const IGNORE_FILES_SUFFIX = ['.log', '.lock']
+
+function findIconByName(fileName: string) {
+  const fileNameList = fileName.split('.')
+  fileNameList.shift()
+  const targetSuffix = fileNameList.join('.')
+  const suffixToIconList = [
+    { suffix: ['ts', 'js'], icon: <IconCode /> },
+    { suffix: ['json'], icon: <IconCodeBlock /> },
+    { suffix: ['md', 'doc'], icon: <IconFilePdf /> },
+    { suffix: ['config.json', 'config.js','d.ts'], icon: <IconCommon /> },
+  ]
+  const res = suffixToIconList.find(({ suffix }) => suffix.some(name => targetSuffix === name))
+  return res?.icon || <IconDriveFile />
+}
+
 function fileListToTree(fileList: Obj[]) {
   const importantFiles = fileList.filter(item => !item.name.startsWith('.')) // Only the important file
   const folders = importantFiles.filter(item => item.isDirectory)
-  const codeFiles = importantFiles.filter(item => !item.isDirectory)
+  const otherFiles = importantFiles.filter(item =>
+    !item.isDirectory &&
+    !IGNORE_FILES_SUFFIX.some(suffix => item.name.endsWith(suffix))
+  )
 
   const foldersTree = folders.map(item => createTreeNode(item, <IconFolder />))
-  const codeFilesTree = codeFiles.map(item => createTreeNode(item, <IconCode />))
+  const codeFilesTree = otherFiles.map(item => createTreeNode(item, findIconByName(item.name)))
   return foldersTree.concat(codeFilesTree)
 }
 
